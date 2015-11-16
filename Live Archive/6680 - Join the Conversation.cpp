6b873ed9aed026aa1c23cf3fecc36aa273d9614a
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2015-11-16 22:25:03 +0000

    New Update

--- Live Archive/6680 - Join the Conversation.cpp
+++ Live Archive/6680 - Join the Conversation.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#define endl '\n'
 #define fast ios_base::sync_with_stdio(false);cin.tie(NULL)
 
 //https://icpcarchive.ecs.baylor.edu/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=4692
@@ -13,77 +14,83 @@ int toInt(string &x){
 	return r;
 }
 
-void dfs(int u,vector< vector<int> > &G,vector<bool> &visited, int &deep){
-	visited[u] = true;
-	deep += 1;
-	for(int i=0; i<G[u].size(); i++){
-		int tmp = G[u][i];
-		if(!visited[tmp]){
-			dfs(tmp,G,visited,deep);
+int find(int node,vector< vector<int> > &G, vector<int> &dist){
+	if(dist[node] != -1)
+		return dist[node];
+
+	int best = 1;
+	for(int i=0; i<G[node].size(); i++){
+		best = max(best, find(G[node][i],G,dist) + 1);
+	}
+
+	return dist[node] = best;
+}
+
+void dfs(int node,vector< vector<int> > &G,vector<int> &dist, vector<int> &ans){
+	for(int i=0; i<G[node].size(); i++){
+		if((find(G[node][i],G,dist) + 1) == dist[node]){
+			ans.push_back(G[node][i]);
+			dfs(G[node][i],G,dist,ans);
+			return;
+		}
+	}
+}
+
+void buildGraph(vector< vector<int> > &G, int n){
+	map<string, vector<int> > decode;
+	string line;
+	for(int k=0; k<n; k++){
+		getline(cin,line);
+
+		int index = line.find(':');
+		string name = line.substr(0,index);
+		index++;
+
+		stringstream ss(line.substr(index));
+		decode[name].push_back(k);
+
+		while(ss >> line){
+			if(line[0] == '@'){
+				if(line == name)continue; // eliminamos ciclos
+				for(int i=0; i<decode[line].size(); i++){
+					G[decode[line][i]].push_back(k); // edges
+				}
+			}
 		}
 	}
 }
 
 int main(){
+	fast;
 	int n;
 	string buffer;
 	while(getline(cin,buffer)){
 		n = toInt(buffer);
-		int id = 0;
-		map<string, int> decode;
-		map<string, int>::iterator it;
-		vector<string> chat(n);
-		string tmp;
 
-		//Mapeamos el string con un id
-		for(int i=0; i<n; i++){
-			getline(cin,tmp);
-			chat[i] = tmp;
-			stringstream ss(tmp);
-			ss >> tmp;
-			it = decode.find(tmp);
-			if(it == decode.end()){
-				decode[tmp] = id;
-				id++;
-			}
-		}
-
-		vector< vector<int> > G(id+1);
-		vector<bool> visited(id+1, false);
-
-		//construimos el grafo a partir del mapeo anteior
-		string node,edges;
-		for(int i=0; i<n; i++){
-			tmp = chat[i];
-			stringstream ss(tmp);
-			ss >> node;
-			while(ss >> edges){
-				if(edges[0] == '@'){
-					G[decode[node]].push_back(decode[edges+":"]);
-				}
-			}
-		}
-
+		vector< vector<int> > G(n);
+		buildGraph(G,n);
+		vector<int> dist(n,-1);
+		vector<int> ans;
+		
 		//Hacemos el dfs dejando la ruta de maxima profundidad
-		int maxi = 0, init = 0, deep = 0;
-		for(int i=0; i < G.size(); i++){
-			if(maxi < G[i].size()){
-				maxi = G[i].size();
-				init = i;
-			}			
+		int best = 0, idx = -1;
+		for(int i=0; i < n; i++){
+			int t = find(i,G,dist);
+			//Buscamos el nodo con la maxima profundidad
+			if(best < t){
+				best = t;
+				idx = i;
+			}
 		}
 
 		//Visitamos los nodos donde la ruta es la de mayor profundidad
-		dfs(init,G,visited, deep);
-
-		printf("%d\n",maxi);
-		for(int i=0; i<visited.size(); i++){
-			if(visited[i]){
-				printf("%d",i+1);
-				if(i<visited.size()-1)printf(" ");
-			}
+		cout<<best<<endl;
+		cout<<idx + 1;
+		dfs(idx,G,dist,ans);
+		for(int i=0; i<ans.size(); i++){
+			cout<<" "<<ans[i]+1;
 		}
-		printf("\n");
+		cout<<endl;
 	}
 	return 0;
 }
