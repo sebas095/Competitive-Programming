fa04310f92487779ff3c7cc9389309ff7285f746
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2015-11-22 03:20:42 +0000

    Graph Practice

---
+++ Hackerearth/Kingdom Of Monkeys_V1.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define endl '\n'
+
+using namespace std;
+
+long long cost = 0;
+
+void dfs(int node, vector<vector<int> >&G, vector<bool> &visited, vector<long long> &bananas){
+	visited[node] = true;
+	cost += bananas[node];
+
+	for(int i = 0; i < G[node].size(); i++){
+		int tmp = G[node][i];
+		if(!visited[tmp])
+			dfs(tmp,G,visited,bananas);
+	}
+}
+
+int main(){
+	fast;
+	int t,n,edges,a,b;
+	cin>>t;
+	while(t--){
+		cin>>n>>edges;
+
+		long long ans = 0;
+		vector<vector<int> >G(n);
+		vector<bool> visited(n,false);
+		vector<long long> bananas(n);
+
+		for(int i=0; i<edges; i++){
+			cin>>a>>b;
+			a--;b--;
+			G[a].push_back(b);
+			G[b].push_back(a);
+		}
+
+		for(int i=0; i<n; i++) cin>>bananas[i];
+
+		for(int i=0; i<n; i++){
+			if(!visited[i]){
+				cost = 0;
+				dfs(i,G,visited,bananas);
+				ans = max(ans,cost);	
+			}
+		}
+
+		cout<<ans<<endl;
+	}
+	return 0;
+}
---
+++ Hackerearth/Kingdom Of Monkeys_V2.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define endl '\n'
+
+using namespace std;
+
+struct Edge{
+	int u;
+	int v;
+	Edge(){}
+	Edge(int _u, int _v): u(_u), v(_v){}
+};
+
+int p[100001]; //arbol para el union-find
+Edge G[100000]; //Grafo
+vector<long long> bananas(100001);
+
+int find_set(int u){
+	if(p[u] == u) return u; //si encontro la raiz en el arbol retorna ese nodo 
+	return p[u] = find_set(p[u]);
+}
+
+void union_set(int u, int v){
+	int pu = find_set(u);
+	int pv = find_set(v);
+
+	if(pu == pv) return; //Si tienen el mismo padre no hacer nada, porque ya estan conectados
+
+	p[pu] = pv; //Los conectamos a un mismo padre
+	bananas[pv] += bananas[pu]; //Modificacion para este problema, 
+								//aqui el padre del arbol guardara el costo acumulado
+}
+
+void init(int n){
+	for(int i=0; i<n; i++){
+		p[i] = i; //Inicializo con que cada nodo es su propio padre inicialmente
+	}
+}
+
+int main(){
+	fast;
+	int t,n,m,a,b;
+	long long ans;
+	cin>>t;
+	while(t--){
+		cin>>n>>m;
+		ans = 0;
+		init(n); //inicializamos nuestro arbol de union-find
+
+		for(int i=0; i<m; i++){
+			cin>>a>>b;
+			a--;b--;
+			G[i].u = a;
+			G[i].v = b;
+		}
+
+		for(int i=0; i<n; i++) cin>>bananas[i];
+
+		for(int i=0; i<m; i++){
+			union_set(G[i].u,G[i].v); //Empezamos a construir el arbol
+		}		
+
+		for(int i=0; i<n; i++){
+			ans = max(ans,bananas[find_set(i)]); //Extraemos el costo acumulado de cada padre,es decir de cada isla
+		}
+
+		cout<<ans<<endl;
+	}
+	return 0;
+}
