5916057107bae8c4f2a171828a693bb5533761e9
Sebastian Duque <sebas_tian_95@hotmail.com>
2016-01-03 00:02:11 -0500

    Practice

---
+++ Codeforces/Unusual Product.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int n, acc = 0, query, qi, value;
+  scanf("%d", &n);
+  int m[n][n];
+
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++) {
+      scanf("%d", &m[i][j]);
+      if (i == j) acc += m[i][j];
+    }
+
+  cin >> query;
+  while (query--) {
+    scanf("%d", &qi);
+    if (qi == 3)
+      cout << (acc & 1);
+    else {
+      scanf("%d", &value);
+      acc++;
+    }
+
+  }
+  cout << endl;
+  return 0;
+}
