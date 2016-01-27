a19f41aab649c5ba6f6273e3c0db0ad85e58dd21
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2016-01-27 00:22:50 -0500

    Update Santa Claus Bag.cpp

--- URI/Santa Claus Bag.cpp
+++ URI/Santa Claus Bag.cpp
@@ -16,30 +16,30 @@ terna memo[110][55];
 bool vi[110][55];
 
 terna DP(int index, int capacity, vector<int> &weights, vector<int> &prices) {
-	if(index == n) return MP(MP(0, 0), capacity);
-	if (vi[index][capacity]) return memo[index][capacity];
+  if(index == n) return MP(MP(0, 0), capacity);
+  if (vi[index][capacity]) return memo[index][capacity];
 
   vi[index][capacity] = true;
-	terna ans1 = DP(index + 1, capacity, weights, prices);
+  terna ans1 = DP(index + 1, capacity, weights, prices);
 
-	if (weights[index] <= capacity) {
+  if (weights[index] <= capacity) {
     terna ans2 = DP(index + 1, capacity - weights[index], weights, prices);
-		ans2.ff.ff += prices[index];
+    ans2.ff.ff += prices[index];
     ans2.ff.ss += 1;
     ans1 = max(ans1, ans2);
   }
 
-	return memo[index][capacity] = ans1;
+  return memo[index][capacity] = ans1;
 }
 
 int main() {
-	fast;
+  fast;
   int t;
   cin >> t;
 
   while (t--) {
     cin >> n;
-	  memset(vi, false, sizeof(vi));
+    memset(vi, false, sizeof(vi));
     vector<int> weights(n), prices(n);
 
     for(int i = 0; i < n; i++)
@@ -50,5 +50,5 @@ int main() {
     cout << "Peso: " << 50 - ans.ss << " kg" << endl;
     cout << "sobra(m) " << n - ans.ff.ss << " pacote(s)" << endl << endl;
   }
-	return 0;
+  return 0;
 }
