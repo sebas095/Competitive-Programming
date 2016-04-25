cbbc9bac21d14363c172665a3cb1f21c21a362d0
Sebastian Duque <sebas_tian_95@hotmail.com>
2016-04-25 00:28:28 -0500

    Training Abr-24-2016

---
+++ URI/Above Average.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define endl '\n'
+
+using namespace std;
+
+int main(){
+  int cases;
+  cin >> cases;
+
+  while(cases--){
+    int N;
+    cin >> N;
+		cout << fixed;
+		cout.precision(3);
+    vector<double> students(N);
+		double average = 0.0, count = 0.0, ans = 0.0;
+
+		for(int i = 0; i < N; i++) cin >> students[i];
+		for(int i = 0; i < N; i++){
+      average += students[i];
+    }
+
+    average /= N;
+		for(int i = 0; i < N; i++){
+      if(students[i] > average) count += 1;
+    }
+
+    ans = (count / (double)N) * 100.000;
+    cout << ans << "%" << endl;
+  }
+
+  return 0;
+}
